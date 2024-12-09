a87eeae2304b13f0286ecb186a4d5cb22816a01b
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-12-09 10:02:58 +0100

    task 1 done

---
+++ utils/db.js
@@ -0,0 +1,61 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}`;
+    
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.dbName = database;
+
+    this.connected = false;
+
+    this.client.connect()
+      .then(() => {
+        // console.log('MongoDB client connected');
+        this.connected = true;
+        this.db = this.client.db(this.dbName);
+      })
+      .catch((err) => {
+        console.error(`MongoDB connection error: ${err}`);
+        this.connected = false;
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    if (!this.connected) {
+      return 0;
+    }
+
+    try {
+      const usersCollection = this.db.collection('users');
+      return usersCollection.countDocuments();
+    } catch (err) {
+      console.error(`Error fetching user count: ${err}`);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    if (!this.connected) {
+      return 0;
+    }
+
+    try {
+      const filesCollection = this.db.collection('files');
+      return filesCollection.countDocuments();
+    } catch (err) {
+      console.error(`Error fetching file count: ${err}`);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
